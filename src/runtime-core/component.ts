f4abafbbe7bad12c25a90942fe694534382159ea
betterlin <betterlin@tencent.com>
2023-02-22 19:10:40 +0800

    feat: getCurrentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -38,9 +38,13 @@ function setupStatefulComponent(instance) {
 
   const { setup } = Component
   if (setup) {
+    setCurrentInstance(instance);
+
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit,
     });
+
+    setCurrentInstance(null);
 
     handleSetupResult(instance, setupResult)
   }
@@ -59,4 +63,14 @@ function finishComponentSetup(instance) {
   const Component = instance.type
 
   instance.render = Component.render
+}
+
+let currentInstance = null;
+
+export function getCurrentInstance() {
+  return currentInstance;
+}
+
+export function setCurrentInstance(instance) {
+  currentInstance = instance;
 }
